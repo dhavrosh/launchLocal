d71b702bfb5b29a2ce44ea00d838a13cb4e537b2
d_havrosh <dhavrosh@gmail.com>
2016-10-31 13:01:59 +0200

    initial commit

---
+++ keystone.js
@@ -0,0 +1,41 @@
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').config();
+
+// Require keystone
+var keystone = require('keystone');
+var cons = require('consolidate');
+var nunjucks = require('nunjucks');
+
+keystone.init({
+	'name': 'Launch Local',
+	'brand': 'Launch Local',
+
+	'less': 'public',
+	'static': 'public',
+	'favicon': 'public/favicon.ico',
+	'views': 'templates/views',
+	'view engine': 'html',
+	'custom engine': cons.nunjucks,
+
+	'auto update': true,
+	'session': true,
+	'auth': true,
+	'user model': 'User',
+});
+keystone.import('models');
+keystone.set('locals', {
+	_: require('lodash'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable,
+});
+keystone.set('routes', require('./routes'));
+keystone.set('nav', {
+	posts: ['posts', 'post-categories'],
+	galleries: 'galleries',
+	enquiries: 'enquiries',
+	users: 'users',
+});
+
+keystone.start();
